cf61c5fbcaefced4aa94a3b6f108f97645279b06
Rutik More <rutvikmore1234567890@gmail.com>
2026-09-20 14:42:59 +0530

    simple if else prog in py

---
+++ ifelse.py
@@ -0,0 +1,23 @@
+#Check whether a number is positive, negative, or zero.
+num = int(input("enter a number "))
+if num > 0:
+    print("number is positive ")
+elif num < 0:
+    print("number is negative")
+else:
+    print("number is ")        
+
+#Check whether a person is eligible to vote based on age.
+
+#Take three numbers and print the largest.
+
+#Create a grading system based on marks:
+
+
+#Check whether a year is a leap year.
+
+#Create a calculator using if-elif-else that supports +, -, *, and /.
+
+#Check whether a character is a vowel or consonant.
+
+#Create a login system that checks a username and password.
