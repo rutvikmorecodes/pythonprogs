cb0e50043971114f902f57156d9dfec2ff35ff34
Rutik More <rutvikmore1234567890@gmail.com>
2026-09-20 21:28:15 +0530

    print 1 to 100 & 1 to 50 even numbers

---
+++ loops.py
@@ -0,0 +1,35 @@
+"""Print numbers from 1 to 100 using a for loop."""
+for i in range(1,101,2):
+
+    print(i)
+  
+
+#Print all even numbers between 1 and 50.
+for i in range(1,50):
+    if i % 2 == 0:
+       print(i)
+
+#Print the multiplication table of a number entered by the user.
+
+#Calculate the sum of numbers from 1 to n using a loop.
+
+#Calculate the factorial of a number using a while loop.
+
+#Reverse a number using a loop. Example: 1234 → 4321.
+
+#Count the number of digits in an integer.
+
+#Check whether a number is prime.
+
+#Print all prime numbers between 1 and 100.
+#Print the Fibonacci series for n terms.
+
+#Print this pattern: triangle right angle 
+#Print this pattern: triangle right angle numbers 
+"""1 
+12
+123"""
+#Find the sum of all elements in a list using a loop, without using sum().
+
+#find the maximum element in a list without using max().
+
