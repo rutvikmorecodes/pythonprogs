998d1b4968e9e061e664b1106bf8cc25d5af358f
Rutik More <rutvikmore1234567890@gmail.com>
2026-09-20 21:48:35 +0530

    sum 1 to n number using loop

--- loops.py
+++ loops.py
@@ -18,6 +18,17 @@ for i in range(1,11):
 
 #Calculate the sum of numbers from 1 to n using a loop.
 
+n = int(input( "enter the number : "))
+sum = 0
+for i in range(1,n + 1):
+    
+
+    sum = sum + i 
+    print(sum)
+
+
+
+
 #Calculate the factorial of a number using a while loop.
 
 #Reverse a number using a loop. Example: 1234 → 4321.
